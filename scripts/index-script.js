fd7d3129853d24c5b3b0826fad42867ee0abbccf
eflaatten <elliotflaatten@live.com>
2024-02-12 23:12:46 -0600

    almost done with homepage. sizing issue to be fixed.

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,14 +1,3 @@
-let galleryPreviews = [
-previewOne = document.getElementById('previewOne'),
-previewTwo = document.getElementsById('previewTwo'),
-previewThree = document.getElementById('previewThree')]
-  
-
-navToGallery = () => {
-  galleryPreviews.forEach((preview) => {
-    preview.addEventListener('click', () => {
-      document.location.href = './pages/gallery.html'
-      console.log("Navigating to gallery.html...")
-    })
-  })
+navigateTo = (url) => {
+  window.location.href = url
 }
